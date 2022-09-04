9444dc41ca83cb469974e1646a6456618196ade7
falerojob <110434984+falerojob@users.noreply.github.com>
2022-09-04 18:25:12 -0300

    Entrega final 2
    asd

--- js/products.js
+++ js/products.js
@@ -2,13 +2,35 @@ let productsArray = [];
 let id_productos = localStorage.getItem("catID")
 let btnAsc = document.getElementById("sortAsc")
 let btnDesc = document.getElementById("sortDesc")
-let btnRel =document.getElementById("sortByCount")
+let btnRel = document.getElementById("sortByCount")
+
+let btnClean = document.getElementById("clearRangeFilter")
+let btnFilter = document.getElementById("rangeFilterPrice")
+
+let inputMin = document.getElementById("rangeFilterPriceMin")
+let inputMax = document.getElementById("rangeFilterPriceMax")
 
 function showProductsList(array){
     let htmlContentToAppend = "";
 
+    let min;
+    if (inputMin.value !== "" && inputMin.value !== undefined){
+        min = inputMin.value;
+    } else {
+        min = 0;
+    };
+
+    let max;
+    if (inputMax.value !== "" && inputMax.value !== undefined){
+        max = inputMax.value;
+    } else {
+        max = Infinity;
+    };
+
     for(let i = 0; i < array.length; i++){ 
         let product = array[i];
+
+        if (product.cost >= min && product.cost <= max){
         htmlContentToAppend += `
         <div class="list-group-item list-group-item-action">
             <div class="row">
@@ -29,6 +51,7 @@ function showProductsList(array){
         </div>
         `
         document.getElementById("prod-list-container").innerHTML = htmlContentToAppend; 
+    }
     }
 }
 
@@ -70,5 +93,15 @@ btnRel.addEventListener("click", function(){
         if(a.soldCount < b.soldCount) {return 1;}
         return 0;
     });
+    showProductsList(productsArray)
+});
+
+btnFilter.addEventListener("click", function(){
+    showProductsList(productsArray)
+});
+
+btnClean.addEventListener("click", function(){
+    inputMin.value = ""
+    inputMax.value = ""
     showProductsList(productsArray)
 });
